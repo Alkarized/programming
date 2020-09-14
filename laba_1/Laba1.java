c6390fd18e958c42f61bb574edd5070efd051414
Mikhail <golananime@gmail.com>
2020-09-14 12:57:31 +0300

    ssss

--- laba_1/Laba1.java
+++ laba_1/Laba1.java
@@ -13,7 +13,7 @@ public class Laba1{
 			for(int j = 0; j < 19; j++){
 				if(d[i]==12){
 					d[i][j] = (double)Math.pow((0.5d)/((double)(Math.PI - 0.25d*(Math.sin(x[j]-1)))),Math.pow(Math.E,Math.cos(x[j])));
-				} else if(d[i] == 4 && d[i] == 6 && d[i] == 8 && d[i] == 18 && d[i] == 20){
+				} else if(d[i] == 4 || d[i] == 6 || d[i] == 8 || d[i] == 18 || d[i] == 20){
 					d[i][j] = (double)Math.asin(Math.sin(Math.atan(1/(Math.pow(Math.E,Math.abs(x[j]))))));
 				} else{
 					d[i][j] = (double)Math.pow((0.75d/(1 - Math.cos(Math.asin(((x[j]-4)/28))))),2);
