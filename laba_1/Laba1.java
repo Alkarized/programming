637403fe1e79fd256393fd9a36662636ccddcd4c
Mikhail <golananime@gmail.com>
2020-09-14 11:14:36 +0300

    final version

--- laba_1/Laba1.java
+++ laba_1/Laba1.java
@@ -12,11 +12,11 @@ public class Laba1{
 		for(int i = 0; i < 11; i++){
 			for(int j = 0; j < 19; j++){
 				if(d[i]==12){
-					dd[i][j] = (float)Math.pow((0.5f)/((float)(Math.PI - 1/4*(Math.sin(x[j]-1)))),Math.pow(Math.E,Math.cos(x[j])));
+					dd[i][j] = (float)Math.pow((0.5f)/((float)(Math.PI - 0.25f*(Math.sin(x[j]-1)))),Math.pow(Math.E,Math.cos(x[j])));
 				} else if(d[i] == 4 && d[i] == 6 && d[i] == 8 && d[i] == 18 && d[i] == 20){
 					dd[i][j] = (float)Math.asin(Math.sin(Math.atan(1/(Math.pow(Math.E,Math.abs(x[j]))))));
 				} else{
-					dd[i][j] = (float)Math.pow((3/4f/(1 - Math.cos(Math.asin(1/2*((x[j]-4)/14))))),2);
+					dd[i][j] = (float)Math.pow((0.75f/(1 - Math.cos(Math.asin(((x[j]-4)/28))))),2);
 				}
 			}
 		}
