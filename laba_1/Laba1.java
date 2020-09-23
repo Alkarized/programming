00dd2954c44524645a2a5e103ae6a6fda69fd163
Mikhail <golananime@gmail.com>
2020-09-23 21:32:00 +0300

    yousee

--- laba_1/Laba1.java
+++ laba_1/Laba1.java
@@ -1,22 +1,21 @@
 import java.lang.*;
-import java.util.*;
 
 public class Laba1{
 	public static void main(String[] args) {
 		long[] d = {4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
 		float[] x = new float[19];
-		for(float a: x){
-			a = (float)(-11.0 + Math.random()*14);	
+		for(int i = 0; i < 19; i++){
+			x[i] = (float)(-11.0 + Math.random()*14);	
 		}
 		double[][] dd = new double[11][19];
 		for(int i = 0; i < 11; i++){
 			for(int j = 0; j < 19; j++){
 				if(d[i]==12){
-					dd[i][j] = (double)Math.pow((0.5d)/((double)(Math.PI - 0.25d*(Math.sin(x[j]-1)))),Math.pow(Math.E,Math.cos(x[j])));
+					dd[i][j] = Math.pow((0.5d)/((Math.PI - 0.25d*(Math.sin(x[j]-1)))),Math.pow(Math.E,Math.cos(x[j])));
 				} else if(d[i] == 4 || d[i] == 6 || d[i] == 8 || d[i] == 18 || d[i] == 20){
-					dd[i][j] = (double)Math.asin(Math.sin(Math.atan(1/(Math.pow(Math.E,Math.abs(x[j]))))));
+					dd[i][j] = Math.asin(Math.sin(Math.atan(1/(Math.pow(Math.E,Math.abs(x[j]))))));
 				} else{
-					dd[i][j] = (double)Math.pow((0.75d/(1 - Math.cos(Math.asin(((x[j]-4)/28))))),2);
+					dd[i][j] = Math.pow((0.75d/(1 - Math.cos(Math.asin(((x[j]-4)/28))))),2);
 				}
 			}
 		}
