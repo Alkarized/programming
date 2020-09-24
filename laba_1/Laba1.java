b559b35b3e464a996344839d3425b48400c14648
Mikhail <golananime@gmail.com>
2020-09-24 18:26:15 +0300

    final

--- laba_1/Laba1.java
+++ laba_1/Laba1.java
@@ -1,4 +1,4 @@
-import java.lang.*;
+/*import static java.lang.*;
 
 public class Laba1{
 	public static void main(String[] args) {
@@ -16,6 +16,38 @@ public class Laba1{
 					dd[i][j] = Math.asin(Math.sin(Math.atan(1/(Math.pow(Math.E,Math.abs(x[j]))))));
 				} else{
 					dd[i][j] = Math.pow((0.75d/(1 - Math.cos(Math.asin(((x[j]-4)/28))))),2);
+				}
+			}
+		}
+		for(int i = 0; i < 11; i++){
+			for(int j = 0; j < 19; j++){
+				System.out.printf("%.3f",dd[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+}
+*/
+import static java.lang.Math.*;
+
+public class Laba1{
+	public static void main(String[] args) {
+		long[] d = {4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
+		float[] x = new float[19];
+		for(int i = 0; i < 19; i++){
+			x[i] = (float)(-11.0 + random()*14);	
+		}
+		double[][] dd = new double[11][19];
+		for(int i = 0; i < 11; i++){
+			for(int j = 0; j < 19; j++){
+				if(d[i]==12){
+					dd[i][j] = pow((0.5d)/((PI - 0.25d*(sin(x[j]-1)))),pow(E,cos(x[j])));
+				} else if(d[i] == 4 || d[i] == 6 || d[i] == 8 || d[i] == 18 || d[i] == 20){
+					dd[i][j] = asin(sin(atan(1/(pow(E,abs(x[j]))))));
+				} else{
+					dd[i][j] = pow((0.75d/(1 - cos(asin(((x[j]-4)/28))))),2);
 				}
 			}
 		}
--- laba_1/cs.sh
+++ laba_1/cs.sh
@@ -1,4 +1,4 @@
 javac Laba1.java
-echo "Main-Class: Laba1" > fest.ms
-jar cvmf fest.ms Laba1.jar Laba1.class
+echo "Main-Class: Laba1" > fest.mf
+jar cvmf fest.mf Laba1.jar Laba1.class
 java -jar Laba1.jar
